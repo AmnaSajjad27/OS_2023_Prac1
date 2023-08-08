d118014e2c1140494eb312aed49fb8b2eb289931
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-08-08 23:09:19 +0930

    what the acutall fuk is on

--- minishell.c
+++ minishell.c
@@ -44,22 +44,27 @@ int i; /* parse index */
 /* prompt for and process one command line at a time */
 // infinite loop keeps running asking for input 
 while (1)
-{ /* do Forever */
+{ 
+    /* do Forever */
 prompt();
 // read in user command and store in line
 fgets(line, NL, stdin);
 fflush(stdin);
 // check if end of file, then exist 
 if (feof(stdin)) 
-{ /* non-zero on EOF */
+{ 
+/* non-zero on EOF */
 // fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
 // feof(stdin), ferror(stdin));
 exit(0);
 }
+
 if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
 continue; /* to prompt */
+
 // v[0] holds the command and v[i] hold its arguments 
 v[0] = strtok(line, sep);
+
 for (i = 1; i < NV; i++) 
 {
 // tokenised into indivual commands 
@@ -67,6 +72,7 @@ v[i] = strtok(NULL, sep);
 if (v[i] == NULL)
 break;
 }
+
 // chekc if its a background command 
 int background_cmd = 0;
 if (i > 1 && strcmp(v[i - 1], "&") == 0) 
@@ -79,24 +85,19 @@ if (i > 1 && strcmp(v[i - 1], "&") == 0)
 // built in command cd - changes work directory/ folder
 if (strcmp(v[0], "cd") == 0)
 {
-    // if user enter cd in command line
-    if (v[1] == NULL)
+    if (i > 1)
     {
-        // no directory specificed 
-        chdir(getenv("Desktop"));
-        perror("chdir");
-        // Testing
-        // printf("no dir specified");
-    }
-    else
-    {
-        if (chdir(v[1]) != 0)
+        // arg given i.e. cd home
+        if (chdir(v[1] == -1))
         {
-            // if directory specified i.e. cd Amnas folder
             perror("cd");
         }
+        else
+        {
+            fprintf(stderr, "cd: missing argument\n");
+        }
+        continue;
     }
-    return 0;
 }
 
 /* assert i is number of tokens + 1 */
@@ -104,15 +105,19 @@ if (strcmp(v[0], "cd") == 0)
 switch (frkRtnVal = fork()) {
 case -1: /* fork returns error to parent process */
 {
-perror("fork");
-break;
+    perror("fork");
+    break;
 }
 case 0: /* code executed only by child process */
 {
 // excute the command 
-execvp(v[0], v);
-perror("execvp");
-exit(EXIT_FAILURE);
+if (execvp(v[0], v) == -1)
+{
+    // terminate if not properly excuted
+    perror("execvp");
+    exit(EXIT_FAILURE);
+}
+break;
 }
 default: /* code executed only by parent process */
 {
@@ -120,15 +125,10 @@ default: /* code executed only by parent process */
     {
         wpid = wait(0);
         // printf("%s done \n", v[0]);
-        break;
     }
-    else
-    {
-        printf("%s is running in the background \n", v[0]);
-        break;
-    }
-return frkRtnVal;
-/* switch */
+    break;
+    return frkRtnVal;
+    /* switch */
 } /* while */
  /* main */
 }
