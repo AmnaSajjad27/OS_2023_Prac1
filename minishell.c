1a2c900710e0c66f8b6117f90dd735a9da9f9cf0
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-08-09 21:27:24 +0930

    background counter

--- minishell.c
+++ minishell.c
@@ -15,66 +15,71 @@ Compiler/System : gcc/linux
 #include <stdlib.h>
 #include <signal.h>
 
+#include <stdbool.h>
+
 #define NV 20 /* max number of command tokens */
 #define NL 100 /* input buffer size */
 
 // input command 
 char line[NL]; /* command input buffer */
 
-/*
-shell prompt
-*/
-// flushes the output stream 
+// to hold the background process count 
+int background_counter = 0;
+
+/* shell prompt, flushes the output stream */
 void prompt(void)
 {
 // fprintf(stdout, "\n msh> ");
 fflush(stdout);
 }
 
+// Main function 
+/* argk - number of arguments
+ argv - argument vector from command line
+ envp - environment pointer */
+
 int main(int argk, char *argv[], char *envp[])
-/* argk - number of arguments */
-/* argv - argument vector from command line */
-/* envp - environment pointer */
 {
 int frkRtnVal; /* value returned by fork sys call */
 int wpid; /* value returned by wait */
 char *v[NV]; /* array of pointers to command line tokens */
 char *sep = " \t\n";/* command line token separators */
 int i; /* parse index */
+
 /* prompt for and process one command line at a time */
 // infinite loop keeps running asking for input 
 while (1)
 { 
     /* do Forever */
-prompt();
-// read in user command and store in line
-fgets(line, NL, stdin);
-fflush(stdin);
-// check if end of file, then exist 
-if (feof(stdin)) 
-{ 
-/* non-zero on EOF */
-// fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
-// feof(stdin), ferror(stdin));
-exit(0);
-}
+    prompt();
+    // read in user command and store in line
+    fgets(line, NL, stdin);
+    fflush(stdin);
+    // check if end of file, then exist 
+    if (feof(stdin)) 
+    { 
+    /* non-zero on EOF */
+    // fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
+    // feof(stdin), ferror(stdin));
+        exit(0);
+    }
+    if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
+        continue; /* to prompt */
 
-if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-continue; /* to prompt */
-
-// v[0] holds the command and v[i] hold its arguments 
-v[0] = strtok(line, sep);
-
-for (i = 1; i < NV; i++) 
-{
-// tokenised into indivual commands 
-v[i] = strtok(NULL, sep);
-if (v[i] == NULL)
-break;
-}
+    // v[0] holds the command and v[i] hold its arguments 
+    v[0] = strtok(line, sep);
+    
+    for (i = 1; i < NV; i++) 
+    {
+        // tokenised into indivual commands 
+        v[i] = strtok(NULL, sep);
+        if (v[i] == NULL)
+            break;
+    }
 
 // chekc if its a background command 
 int background_cmd = 0;
+
 if (i > 1 && strcmp(v[i - 1], "&") == 0) 
 {
     background_cmd = 1;
@@ -87,50 +92,59 @@ if (strcmp(v[0], "cd") == 0)
 {
     if (i > 1)
     {
+        if (strcmp(v[1], "..") == 0)
+        {
+            chdir("..");
+        }
         // arg given i.e. cd home
-        if (chdir(v[1] == -1))
+        else if (chdir(v[1] == -1))
         {
             perror("cd");
         }
-        else
-        {
-            fprintf(stderr, "cd: missing argument\n");
-        }
-        continue;
     }
+    else
+    {
+        fprintf(stderr, "cd: missing argument\n");
+    }
+    continue;
 }
 
 /* assert i is number of tokens + 1 */
 /* fork a child process to exec the command in v[0] */
-switch (frkRtnVal = fork()) {
-case -1: /* fork returns error to parent process */
+
+switch (frkRtnVal = fork()) 
 {
-    perror("fork");
-    break;
-}
-case 0: /* code executed only by child process */
-{
-// excute the command 
-if (execvp(v[0], v) == -1)
-{
-    // terminate if not properly excuted
-    perror("execvp");
-    exit(EXIT_FAILURE);
-}
-break;
-}
-default: /* code executed only by parent process */
-{
-    if (!background_cmd)
+    case -1: /* fork returns error to parent process */
     {
-        wpid = wait(0);
-        // printf("%s done \n", v[0]);
+        perror("fork");
+        break;
+    }
+    case 0: /* code executed only by child process */
+    {
+    // excute the command 
+    if (execvp(v[0], v) == -1)
+    {
+        // terminate if not properly excuted
+        perror("execvp");
+        exit(EXIT_FAILURE);
     }
     break;
-    return frkRtnVal;
-    /* switch */
-} /* while */
- /* main */
+    }
+    default: /* code executed only by parent process */
+    {
+        if (background_cmd)
+        {
+            background_counter++;
+            printf("[%d] pid\n", background_counter);
+        }
+        else
+        {
+            wpid = wait(0);
+            printf("[%d]+ Done %s\n", background_counter, v[0]);
+        }
+        break;
+        // return frkRtnVal;
+    }
 }
 }
 }
