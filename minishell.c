83561320906649245992414e7da3609e147c9268
AmnaSajjad27 <100545645+AmnaSajjad27@users.noreply.github.com>
2023-08-12 11:45:47 +0930

    fixed simpilified

--- minishell.c
+++ minishell.c
@@ -14,53 +14,47 @@ Compiler/System : gcc/linux
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <stdbool.h>
 
-// #include <stdbool.h>
 
 #define NV 20 /* max number of command tokens */
 #define NL 100 /* input buffer size */
 
-// input command 
-char line[NL]; /* command input buffer */
+char line[NL];
+int processCounter = 0;
+int child_Pids[100];
+int children_stat;
 
-// to hold the background process count 
-int background_counter = 1;
-
-/* shell prompt, flushes the output stream */
-void prompt(void)
+void child_finished()
 {
-    // fprintf(stdout, "\n msh> ");
-    fflush(stdout);
+    for (int i = 0; i < processCounter; i++)
+    {
+        if (waitpid(child_Pids[i], &children_stat, WNOHANG) != 0)
+        {
+            printf("[%d]+ Done                     command_name\n",i+1);
+        }
+    }
 }
-
-// Main function 
-/* argk - number of arguments
- argv - argument vector from command line
- envp - environment pointer */
 
 int main(int argk, char *argv[], char *envp[])
 {
 int frkRtnVal; /* value returned by fork sys call */
-int wpid; /* value returned by wait */
+// int wpid; /* value returned by wait */
 char *v[NV]; /* array of pointers to command line tokens */
 char *sep = " \t\n";/* command line token separators */
 int i; /* parse index */
 
-/* prompt for and process one command line at a time */
-// infinite loop keeps running asking for input 
 while (1)
 { 
-    /* do Forever */
-    prompt();
+    fflush(stdout);
     // read in user command and store in line
     fgets(line, NL, stdin);
     fflush(stdin);
+
     // check if end of file, then exist 
     if (feof(stdin)) 
     { 
     /* non-zero on EOF */
-    // fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
-    // feof(stdin), ferror(stdin));
         exit(0);
     }
     if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
@@ -70,7 +64,6 @@ while (1)
 
     // v[0] holds the command and v[i] hold its arguments 
     v[0] = strtok(line, sep);
-    
     for (i = 1; i < NV; i++) 
     {
         // tokenised into indivual commands 
@@ -80,124 +73,50 @@ while (1)
     }
 
 // chekc if its a background command 
-int background_cmd = 0;
+bool background_cmd = false;
 
 if (i > 1 && strcmp(v[i - 1], "&") == 0) 
 {
-    background_cmd = 1;
+    background_cmd = true;
     v[i - 1] = NULL; // Remove the "&" from token
-    i--; // minus the # of tokens
+    // i--; // minus the # of tokens
 }
 
-// added lines 103 to 121 to ensure correct printintg for multiple background commands
-// Background processes handling
-if (background_counter > 0)
+// cd implementation 
+if (i > 1 && strcmp(v[0], "cd") == 0 && !background_cmd)
 {
-    int stat;
-    int res;
-
-    while ((res = waitpid(-1, &stat, WNOHANG)) > 0)
-    {
-        if (WIFEXITED(stat) || WIFSIGNALED(stat))
-        {
-            // Find the command associated with the completed background process
-            int cmd_index = -1;
-            for (int bg_idx = 0; bg_idx < i; bg_idx++)
-            {
-                if (strcmp(v[bg_idx], "&") == 0)
-                {
-                    cmd_index = bg_idx - 1;
-                    break;
-                }
-            }
-
-            if (cmd_index >= 0)
-            {
-                printf("[%d]+ Done                     ", background_counter);
-                // Reconstruct and print the full command with arguments
-                for (int arg_idx = 0; v[arg_idx] != NULL; arg_idx++)
-                {
-                    printf("%s ", v[arg_idx]);
-                }
-                printf("\n");
-                fflush(stdout);
-                background_counter--;
-            }
-        }
-        else if (res == 0)
-        {
-            // No more background processes have finished
-            break;
-        }
-    }
-}
-
-// built in command cd - changes work directory/ folder
-// check if cd has atleast one argument 
-if (strcmp(v[0], "cd") == 0)
-{
-    if (i > 1)
-    {
-        if (strcmp(v[1], "..") == 0)
-        {
-            if (chdir("..") == -1)
-            {
-                perror("cd");
-            }
-        }
-        // arg given i.e. cd home
-    else if (chdir(v[1]) == -1)
-    {
-        perror("cd");
-    }
+    chdir(v[1]);
     continue;
-    }
 }
-/* assert i is number of tokens + 1 */
-/* fork a child process to exec the command in v[0] */
+
 
 switch (frkRtnVal = fork()) 
 {
     case -1: /* fork returns error to parent process */
     {
-        perror("fork");
+        // perror("fork");
         break;
     }
     case 0: /* code executed only by child process */
     {
-        // excute the command 
-        if (execvp(v[0], v) == -1)
-            {
-                // terminate if not properly excuted
-                perror("execvp");
-                _exit(EXIT_FAILURE);
-            }
-        break;
+        execvp(v[0], v);
     }
     default: /* code executed only by parent process */
     {
+        signal(SIGCHLD, child_finished);
         if (background_cmd)
         {
-            background_counter++;
-            printf("[%d] %d\n", background_counter, frkRtnVal);
-            fflush(stdout);
+            child_finished();
+            wait(0);
+            break;
         }
-        else
+        else 
         {
-            // changed wait 0 to &wpid
-            wpid = waitpid(frkRtnVal, NULL, 0);
-            printf("[%d]+ Done                     ", background_counter);
-            
-        // Reconstruct and print the full command with arguments
-        for (int arg_idx = 0; v[arg_idx] != NULL; arg_idx++)
-        {
-            printf("%s ", v[arg_idx]);
+            child_finished();
+            processCounter++;
+            printf("[%d] %d\n", processCounter, frkRtnVal);
+            child_Pids[processCounter] = frkRtnVal;
         }
-        printf("\n");
-        fflush(stdout);
-        }
-        // break;
-        // return frkRtnVal;
     }
 }
 }
